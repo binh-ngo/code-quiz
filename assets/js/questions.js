38afed114da309675fcc846c108f2d3e9bd45a3b
Binh Ngo <bnngo97@gmail.com>
2022-12-29 11:29:17 -0800

    created html file

---
+++ assets/js/questions.js
@@ -0,0 +1,63 @@
+//questions
+var myQuestions = [
+	{
+		question: "What scope does a variable within a function have?",
+		answers: {
+			a: 'Global',
+			b: 'Inner',
+			c: 'Local',
+            d: 'Child'
+		},
+		correctAnswer: 'c'
+	},
+	{
+		question: "What kind of loop keeps running through a code block as long as the condition remains true?",
+		answers: {
+			a: 'Conditional',
+			b: 'For',
+			c: 'Else',
+            d: 'While'
+		},
+		correctAnswer: 'b'
+	},
+	{
+		question: "What is a block of code with a purpose of fulfilling a specific task?",
+		answers: {
+			a: 'Variable',
+			b: 'String',
+			c: 'Declaration',
+            d: 'Function'
+		},
+		correctAnswer: 'd'
+	},
+	{
+		question: "What element is used to store many values in one variable?",
+		answers: {
+			a: 'Functions',
+			b: 'Constants',
+			c: 'Arrays',
+            d: 'Strings'
+		},
+		correctAnswer: 'c'
+	},
+	{
+		question: "What is used that is hidden in code that makes the code more readable?",
+		answers: {
+			a: 'Tips',
+			b: 'Quotes',
+			c: 'Comments',
+            d: 'Help'
+		},
+		correctAnswer: 'c'
+	},
+	{
+		question: "Which of these are not events?",
+		answers: {
+			a: 'Click',
+			b: 'Hover',
+			c: 'DblClick',
+            d: 'Slide'
+		},
+		correctAnswer: 'd'
+	},
+];
