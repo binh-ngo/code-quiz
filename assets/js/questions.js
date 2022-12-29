fce3525aaf6260dea0076c9fc1118202e83c85d8
Binh Ngo <bnngo97@gmail.com>
2022-12-29 11:46:54 -0800

    created a separate js file containing my questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,63 +1,53 @@
 //questions
 var myQuestions = [
 	{
-		question: "What scope does a variable within a function have?",
-		answers: {
-			a: 'Global',
-			b: 'Inner',
-			c: 'Local',
-            d: 'Child'
-		},
-		correctAnswer: 'c'
+		question: "What is the rarest M&M color?",
+        answers:["blue", "yellow", "green", "brown"],
+        correctAnswer:"brown"
 	},
 	{
-		question: "What kind of loop keeps running through a code block as long as the condition remains true?",
-		answers: {
-			a: 'Conditional',
-			b: 'For',
-			c: 'Else',
-            d: 'While'
-		},
-		correctAnswer: 'b'
+		question: "What is the common name for dried plums?",
+        answers:["raisins", "prunes", "dates", "apricots"],
+        correctAnswer:"brown"
 	},
 	{
-		question: "What is a block of code with a purpose of fulfilling a specific task?",
-		answers: {
-			a: 'Variable',
-			b: 'String',
-			c: 'Declaration',
-            d: 'Function'
-		},
-		correctAnswer: 'd'
+		question: "Which country consumes the most chocolate per capita?",
+        answers:["u.s.", "belgium", "switzerland", "france"],
+        correctAnswer:"switzerland"
 	},
 	{
-		question: "What element is used to store many values in one variable?",
-		answers: {
-			a: 'Functions',
-			b: 'Constants',
-			c: 'Arrays',
-            d: 'Strings'
-		},
-		correctAnswer: 'c'
+		question: "What is the name given to Indian food cooked over charcoal in a clay oven?",
+        answers:["tikka", "tandoori", "biryani", "naan"],
+        correctAnswer:"tandoori"
 	},
 	{
-		question: "What is used that is hidden in code that makes the code more readable?",
-		answers: {
-			a: 'Tips',
-			b: 'Quotes',
-			c: 'Comments',
-            d: 'Help'
-		},
-		correctAnswer: 'c'
+		question: "What was the first soft drink in space?",
+        answers:["sprite", "coca cola", "pepsi", "mountain dew"],
+        correctAnswer:"coca cola"
 	},
 	{
-		question: "Which of these are not events?",
-		answers: {
-			a: 'Click',
-			b: 'Hover',
-			c: 'DblClick',
-            d: 'Slide'
-		},
-		correctAnswer: 'd'
+		question: "What is the most consumed manufactured drink in the world?",
+        answers:["coca cola", "coffee", "tea", "pepsi"],
+        correctAnswer:"tea"
+	},
+	{
+		question: "Which is the only edible food that never goes bad?",
+        answers:["honey", "bread", "oil", "mayo"],
+        correctAnswer:"honey"
+	},
+	{
+		question: "Which country invented ice cream?",
+        answers:["china", "france", "u.s.", "italy"],
+        correctAnswer:"china"
+	},
+	{
+		question: "From which country does Gouda cheese originate?",
+        answers:["france", "switzerland", "netherlands", "denmark"],
+        correctAnswer:"netherlands"
+	},
+	{
+		question: "Hendricks, Larios, and Seagram's are some of the best selling brands of which spirit?",
+        answers:["gin", "rum", "vodka", "whiskey"],
+        correctAnswer:"gin"
 	},
 ];
