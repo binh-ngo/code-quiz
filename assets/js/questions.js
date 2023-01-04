7ea665a99831a472d32e51b2e6f10d38a7789e1b
Binh Ngo <bnngo97@gmail.com>
2023-01-04 10:19:00 -0800

    finalized html

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,53 +1,53 @@
 //questions
-var myQuestions = [
+var questions = [
 	{
 		question: "What is the rarest M&M color?",
-        answers:["blue", "yellow", "green", "brown"],
-        correctAnswer:"brown"
+        answers:["Blue", "Yellow", "Green", "Brown"],
+        correctAnswer: "Brown"
 	},
 	{
 		question: "What is the common name for dried plums?",
-        answers:["raisins", "prunes", "dates", "apricots"],
-        correctAnswer:"brown"
+        answers:["Raisins", "Prunes", "Dates", "Apricots"],
+        correctAnswer: "Prunes"
 	},
 	{
 		question: "Which country consumes the most chocolate per capita?",
-        answers:["u.s.", "belgium", "switzerland", "france"],
-        correctAnswer:"switzerland"
+        answers:["U.S.", "Belgium", "Switzerland", "France"],
+        correctAnswer: "Switzerland"
 	},
 	{
 		question: "What is the name given to Indian food cooked over charcoal in a clay oven?",
-        answers:["tikka", "tandoori", "biryani", "naan"],
-        correctAnswer:"tandoori"
+        answers:["Tikka", "Tandoori", "Biryani", "Naan"],
+        correctAnswer: "Tandoori"
 	},
 	{
 		question: "What was the first soft drink in space?",
-        answers:["sprite", "coca cola", "pepsi", "mountain dew"],
-        correctAnswer:"coca cola"
+        answers:["Sprite", "Coca Cola", "Pepsi", "Mountain Dew"],
+        correctAnswer: "Coca Cola"
 	},
 	{
 		question: "What is the most consumed manufactured drink in the world?",
-        answers:["coca cola", "coffee", "tea", "pepsi"],
-        correctAnswer:"tea"
+        answers:["Coca Cola", "Coffee", "Tea", "Pepsi"],
+        correctAnswer: "Tea"
 	},
 	{
 		question: "Which is the only edible food that never goes bad?",
-        answers:["honey", "bread", "oil", "mayo"],
-        correctAnswer:"honey"
+        answers:["Honey", "Bread", "Oil", "Mayo"],
+        correctAnswer: "Honey"
 	},
 	{
 		question: "Which country invented ice cream?",
-        answers:["china", "france", "u.s.", "italy"],
-        correctAnswer:"china"
+        answers:["China", "France", "U.S.", "Italy"],
+        correctAnswer: "China"
 	},
 	{
 		question: "From which country does Gouda cheese originate?",
-        answers:["france", "switzerland", "netherlands", "denmark"],
-        correctAnswer:"netherlands"
+        answers:["France", "Switzerland", "Netherlands", "Denmark"],
+        correctAnswer: "Netherlands"
 	},
 	{
 		question: "Hendricks, Larios, and Seagram's are some of the best selling brands of which spirit?",
-        answers:["gin", "rum", "vodka", "whiskey"],
-        correctAnswer:"gin"
+        answers:["Gin", "Rum", "Vodka", "Whiskey"],
+        correctAnswer: "Gin"
 	},
 ];
